36353e67ca8d93f2eb7a8178410e7ddeea4367a0
sirily11 <sirily1997@gmail.com>
2022-02-17 23:51:29 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import simpleGit from "simple-git";
 
 (async () => {
   const git = simpleGit();
+  await git.checkout("test");
   await git.add("./*");
   await git.commit("Update dependencies");
   await git.push(["-u", "origin", "test"]);
